296ac813cadb5f6519d1e854075308fe73c9717c
John Arnold Tuquero <buboyarnold16t@gmail.com>
2026-07-10 15:00:07 +0800

    Practicing

---
+++ Variables and Data Types/Variables.java
@@ -0,0 +1,32 @@
+
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("Basic Data Types:");
+        System.out.println("- int(integer)");
+        System.out.println("- float(decimal)");
+        System.out.println("- double(long decimal)");  
+        System.out.println("- boolean(True or False)");
+        System.out.println("- char(Letter)");
+
+
+        //declaring and assigning
+        String fullName = "John Arnold C. Tuquero", deviceName = "REALME";
+        int age = 18;
+        float grade = 1.25f;     //putting f at the end to declare
+        double price = 5676.8;
+        char markGrade = 'A';     // using ''
+        boolean truth = true;
+
+
+        System.out.println("\n----------EXAMPLE----------\n");
+
+        System.out.println("buboy");
+        System.out.println("Real Name: " + fullName);
+        System.out.println("John;s age: " + age);
+        System.out.println("John;s grade in programming: " + grade);
+        System.out.println("John;s rate in all class: " + markGrade);
+        System.out.println("John;s device: " + deviceName + ", how much is it? " + price);
+        System.out.println("Do John like baddie's? " + truth);
+
+    }
+}
