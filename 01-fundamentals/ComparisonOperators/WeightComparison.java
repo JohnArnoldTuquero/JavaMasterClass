a592d779bbc555a49073cf4801aaf83ae2d894b3
John Arnold Tuquero <johnarnoldctuquero16@gmail.com>
2026-07-30 11:08:06 +0800

    finishing comparison operator

--- 01-fundamentals/ComparisonOperators/WeightComparison.java
+++ 01-fundamentals/ComparisonOperators/WeightComparison.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class WeightComparison {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("---Weight Comparison---");
+
+        System.out.println("Enter your current weight: ");
+        double initialWeight = sc.nextDouble();
+
+        System.out.println("Enter your target weight: ");
+        double targetWeight = sc.nextDouble();
+
+        if ( initialWeight < targetWeight ) {
+            System.out.println("You still have not achieve the target weight.");
+        } else if ( initialWeight == targetWeight ) {
+            System.out.println("Congratulations!!! you achieve your taret weight!!!");
+        } else {
+            System.out.println("you exceeded your target weight!");
+        }
+
+        sc.close();
     }
 }
